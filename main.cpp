aa437601808c4b91d8f01503ceec35280d27a889
Dungeon_Master <vl_gladskoy@mail.ru>
2021-03-22 13:42:30 +0300

    +main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>  
+#include <string>  
+#include <locale.h> 
+#include <time.h> 
+#include <random> 
+#include <iomanip> 
+#include <chrono> 
+#include <conio.h> 
+  
+using namespace std; 
+
+int main() 
+{
+}
